107c52d1d7ff1956f1cdab73aa14915ecd38088c
mrlonely <mrlonely1226@163.com>
2025-01-25 15:10:03 +0800

    feat: md

--- fullstack/mameikagou/week1/first-dapp/src/main.tsx
+++ fullstack/mameikagou/week1/first-dapp/src/main.tsx
@@ -2,9 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { SuiClientProvider, WalletProvider } from "@mysten/dapp-kit";
+import { networkConfig } from "./networkConfig.ts";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
-		<App />
+		<SuiClientProvider networks={networkConfig} defaultNetwork="testnet">
+			<WalletProvider autoConnect>
+				<App />
+			</WalletProvider>
+		</SuiClientProvider>
 	</React.StrictMode>
 );
